27a6fef9e74a110af58966361b042399cf5a4641
NineTheDivine <1567wkddnjs@gmail.com>
2025-09-10 12:47:11 +0900

    [Feat] Grass, Dirt, WetDirt Tiles
    + Added : Grass, Dirt, WetDirt의  각 상호작용 양식 스크립트
    + Added : Grass, Dirt, WetDirt의 스크립터블 오브젝트 데이터

---
+++ Assets/Scripts/Map/TileInteraction/WetDirtTileBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WetDirtTileBehaviour : ITileInteraction
+{
+    public override TileState Interaction(int tool)
+    {
+        return null;
+    }
+}
