b6a91cae129dd83dc987f9ce6651803bd1971d67
gwangho69 <ckrhkdgh22@gmail.com>
2025-09-10 11:19:46 +0900

    [feat] 플레이어 추가 외 4개 작업
    [+Add] 플레이어 오브젝트 추가
    [+Add] 인풋 시스템 생성
    [+Add] PlayerController 스크립트 생성
    [+Add] 플레이어 애니메이션 작업(부울, 트리거 생성)
    [+Add] 토끼 정지,이동 스프라이트 분할, 오브젝트에 적용

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Mathematics;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Input System")] // 인풋시스템 연결
+    public InputActionReference moveAction;
+    public InputActionReference interactAction;
+
+    [Header("Common")]
+    public float moveSpeed = 2f; // 플레이어 이동속도
+
+    public Rigidbody2D rigidbody;
+
+    Vector2 moveInput;
+
+    private void OnEnable()
+    {
+        if (moveAction != null) moveAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        if (moveAction != null) moveAction.action.Disable();
+    }
+
+    private void Update()
+    {
+        if (moveAction == null) return;
+        moveInput = moveAction.action.ReadValue<Vector2>(); // 이동 구현
+    }
+
+    private void FixedUpdate()
+    {
+        if (rigidbody != null)
+        {
+            Vector2 v = moveInput;
+            if (v.sqrMagnitude > 1f) v = v.normalized; // 대각선 이동 구현
+            rigidbody.velocity = v * moveSpeed;
+        }
+    }
+}
