51e6539b055abb31339a38ed261ac92a43039de7
nakyung71 <nakyung71@naver.com>
2025-09-11 20:52:30 +0900

    [Feat] 신규 에셋 추가
    + Add SunnySide Asset 추가

---
+++ Assets/Scripts/UI/UIQuickSlot.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIQuickSlot : MonoBehaviour,IDropHandler,IPointerClickHandler
+{
+    public Equipment slotEquipment;
+    UIToolBar toolbar;
+    public DragDrop dragDropEquipment;
+    public Outline outline;
+
+    
+
+    public void Init()
+    {
+        dragDropEquipment = GetComponentInChildren<DragDrop>();
+        dragDropEquipment.SetDragDrop(this);
+        outline = GetComponent<Outline>();
+        toolbar=GetComponentInParent<UIToolBar>();
+    }
+
+
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag != null)
+        {
+            
+            eventData.pointerDrag.transform.SetParent(this.transform);
+            eventData.pointerDrag.transform.localPosition = Vector3.zero;
+            slotEquipment = eventData.pointerDrag.GetComponent<DragDrop>().equipment;
+
+            
+        }
+        
+
+        //만약 기존에 이 칸에 오브젝트가 있으면 자리를 바꿔준다.
+        //이제 여기에 더불어서 데이터도 바꿔주는 작업이 필요
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        toolbar.SelectSlot(this);
+    }
+}
