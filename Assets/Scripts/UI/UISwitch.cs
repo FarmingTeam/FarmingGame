c448dcb2f85245819df0c953e5c638b0c1ae3c18
gwangho69 <ckrhkdgh22@gmail.com>
2025-09-17 12:53:51 +0900

    [feat] 인풋 키 추가 외
    [+Added] UI 인벤토리 창 끄꼬 키는 기능 구현 중
    [+ Added] 테스트 씬 갱신

---
+++ Assets/Scripts/UI/UISwitch.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UISwitch : MonoBehaviour
+{
+
+    // 탭키 인풋 따로 참조
+    public InputActionReference inventoryAction;
+    public PlayerInput playerInput;
+
+    public PlayerController PlayerController;
+
+    private bool isOpen;
+
+    private void Awake()
+    {
+
+        if (!PlayerController)
+        {
+            PlayerController = FindObjectOfType<PlayerController>();
+        }
+
+        if (!playerInput)
+        {
+            playerInput = GetComponentInParent<PlayerInput>();
+        }
+    }
+
+    private void OnEnable()
+    {
+        // 인벤토리 활성화
+        if (inventoryAction != null)
+        {
+            inventoryAction.action.performed += ActiveInventory;
+        }
+    }
+
+    private void OnDisable()
+    {
+        // 인벤토리 비활성화
+        if (inventoryAction != null)
+        {
+            inventoryAction.action.performed -= ActiveInventory;
+        }
+    }
+
+    private void ActiveInventory(InputAction.CallbackContext ctx)
+    {
+        Toggle();
+    }
+
+    public void Toggle()
+    {
+        
+    }
+
+    // Inventory 인풋을 받아서 인벤토리 창을 끄고 킵니다.
+    // 인벤토리 창이 켜져있는 동안 플레이어의 동작은 멈춥니다.
+
+}
